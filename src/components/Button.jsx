c47cf913b468c01343956c2470b30828b6450755
Metin <misaxanli@gmail.com>
2024-08-11 16:43:44 +0300

    button-name

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,8 @@
 const Button = ({ onClick, children, className }) => {
   return (
     <button
+      id="al"
+      aria-label={children}
       onClick={onClick}
       className={`px-4 py-2 rounded transition-colors duration-300 ${className}`}>
       {children}

51fa682d05ba0fb68aa410caf6c57ec60ded4c33
Metin <misaxanli@gmail.com>
2024-11-07 15:51:15 +0400

    minor refactor

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -4,40 +4,54 @@ import { useLocation, Link } from "react-router-dom";
 const DynamicBreadcrumb = () => {
   const location = useLocation();
   const [breadcrumbs, setBreadcrumbs] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const pathnames = location.pathname
-      .replace("/YouTube-Converter/", "")
-      .split("/")
-      .filter((x) => x);
-    const newBreadcrumbs = pathnames.map((_, index) => {
-      const routeTo = `/YouTube-Converter/${pathnames.slice(0, index + 1).join("/")}`;
-      return { path: routeTo, label: pathnames[index] };
-    });
-    setBreadcrumbs(newBreadcrumbs);
+    try {
+      const pathnames = location.pathname.split("/").filter(Boolean);
+
+      if (!pathnames.length) {
+        setBreadcrumbs([]);
+        return;
+      }
+
+      const newBreadcrumbs = pathnames.map((_, index) => {
+        const route = `/${pathnames.slice(0, index + 1).join("/")}`;
+        return { path: route, label: pathnames[index] };
+      });
+
+      setBreadcrumbs(newBreadcrumbs);
+    } catch (err) {
+      console.error("Error generating breadcrumbs:", err);
+      setError("An error occurred while generating breadcrumbs.");
+    }
   }, [location]);
 
   return (
-    <nav className="text-blue-400">
+    <nav>
       <Link
         title="Home page"
         aria-label="Home page"
         to="/"
-        className="text-green-500 underline w-fit hover:text-green-600 duration-300 text-sm md:text-base">
+        className="text-[#4CAF50] underline w-fit hover:text-[#236e26] text-sm md:text-base">
         Home
       </Link>
-      {breadcrumbs.map((breadcrumb, index) => (
-        <React.Fragment key={index}>
-          <span className="mx-2 py-2 text-green-500 duration-300 text-sm md:text-base">{">"}</span>
-          <Link
-            title={breadcrumb.label}
-            aria-label={breadcrumb.label}
-            className="text-green-500 underline w-fit hover:text-green-600 duration-300 text-sm md:text-base"
-            to={breadcrumb.path}>
-            {breadcrumb.label}
-          </Link>
-        </React.Fragment>
-      ))}
+      {error ? (
+        <span className="text-red-500 ml-4 text-sm md:text-base">{error}</span>
+      ) : (
+        breadcrumbs.map((breadcrumb, index) => (
+          <React.Fragment key={index}>
+            <span className="mx-2 py-2 text-[#4CAF50] text-sm md:text-base">{">"}</span>
+            <Link
+              title={breadcrumb.label}
+              aria-label={breadcrumb.label}
+              className="text-[#4CAF50] underline w-fit hover:text-[#236e26] text-sm md:text-base"
+              to={breadcrumb.path}>
+              {breadcrumb.label}
+            </Link>
+          </React.Fragment>
+        ))
+      )}
     </nav>
   );
 };
